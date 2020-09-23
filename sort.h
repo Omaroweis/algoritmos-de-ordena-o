dff51cf41592f0edd8e67eca71e792906059ec6f
Omar <omararaujolopes1@gmail.com>
2020-09-23 02:47:12 -0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+#include<iostream>
 
 
 class Sort
@@ -8,6 +9,8 @@ private:
   static int particao(int *vet, int inicio, int fim);
   static void merge(int *vet, int f, int m, int l);
   static void max_heapify(int *vet, int n, int i);
+  static void counting(int *vet, int n, int k);
+  static int getMax(int *vet, int n);
 public:
   Sort();
   static void selectionSort( int *vet, int tam);
@@ -16,6 +19,8 @@ public:
   static void quickSort(int *vet, int inicio, int final);
   static void make_heap(int *vet, int n);
   static void insertionSort(int *vet, int n);
+  static void radixSort(int *vet, int n);
+  static void printVet(int *vet, int n){for(int i=0;i<n;i++) std::cout<<vet[i]<<" "; std::cout<<std::endl;}
 
 
 };
